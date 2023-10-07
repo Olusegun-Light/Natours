6e073dbbe5657eacea71b94fb82ff34adaa2fcd1
Light <lightolusegun1@gmail.com>
2023-10-08 00:32:06 +0100

    cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+const cors = require("cors");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -19,10 +20,17 @@ const bookingRouter = require("./routes/bookingRoutes");
 
 const app = express();
 
+app.enable("trust proxy");
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 // Global Middleware
+
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+app.options("*", cors());
 
 //  Serving static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -71,7 +79,7 @@ app.use(
 );
 
 //  Serving static files
-// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.use(compression());
 
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12774,7 +12774,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9768" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13605" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
